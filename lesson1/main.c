8bfdba1e9aed36a1b78463e86d556a38c14c6f40
Hrishikesh Karande <hrishikeshkarande1997@gmail.com>
2025-04-06 16:54:08 +0200

    comment added in main.c

--- lesson1/main.c
+++ lesson1/main.c
@@ -1,5 +1,9 @@
 int main()
 {       
+    /*
+    We are using the IAR on the MSP430G2453 (Value Line Launch Pad Development Tool)
+    According to the video we can only go as high as 0x7fff for the register15 as the 430 is 16 bit CPU register.
+    */
     int counter = 0;
     ++counter;
     ++counter;
